264c8256602b32012de4c4d44bcc5604255346fc
hibasalem <salemhiba.hs@gmail.com>
2021-07-07 20:34:54 +0300

    finished cc09

---
+++ javascript/linked-list-reverse/README.md
@@ -0,0 +1,58 @@
+# Singly Linked List
+
+[**Pull Request**](https://github.com/hibasalem/data-structures-and-algorithms/pull/34)
+
+[**the excle sheet**](https://docs.google.com/spreadsheets/d/1qGSyKFy6K-d6bPJEMRshfZRl3mYMsCYlBHDFbGCdQfU/edit#gid=0)
+
+Implementation of the linked lists
+
+## Challenge
+
+creating classes for each of the LinkedList and the node including methods for searching, inserting and turning it to string on the list then perform unit tests on each class
+
+## Approach & Efficiency
+
+- code chalange 05
+  `insert()` time : o(1) , space O(n)  
+  `includes()` time : o(n) , space O(n)  
+  `toString()` time : o(n) , space O(n)
+
+- code chalange 06  
+  `append()` time : o(n) , space O(n)  
+  `insertBefore()` time : o(n) , space O(n)  
+  `insertAfter()` time : o(n) , space O(n)
+
+- code chalange 07  
+  `kthFromEnd()` time : o(n) , space O(n)
+
+- code chalange 08  
+  `zipLists()` time : o(n) , space O(n)
+
+- code chalange 09  
+  `reverse()` time : o(n) , space O(n)
+
+![cc07](cc09.jpg)
+
+- code chalange 05
+
+  - **insert** : adds to the head of the link list
+  - **includes** : serches for a value in the linked list
+  - **insert** : return the valuse in the liked list on a string
+
+- code chalange 06
+
+  - **append** : adds to the end of the link list
+  - **insertBefore** : serches for a value in the linked list and adds before it
+  - **insertAfter** : serches for a value in the linked list and adds after it
+
+- code chalange 07
+
+  - **kthFromEnd** : returns the value of a given index from the end of alinked list
+
+- code chalange 08
+
+  - **zipLists** : Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the head of the zipped list.
+
+- code chalange 09
+
+  - **reverse** : revierse a linked list
---
+++ javascript/linked-list-reverse/linked-list-reverse.js
@@ -0,0 +1,142 @@
+"use strict";
+
+const Node = require("./node");
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    const node = new Node(value);
+    if (this.head) {
+      node.next = this.head;
+    }
+    this.head = node;
+  }
+
+  includes(value) {
+    let result;
+    let tempNode = this.head;
+    while (tempNode.next) {
+      if (value == tempNode.value) {
+        result = true;
+        return result;
+      } else {
+        tempNode = tempNode.next;
+      }
+    }
+    result = false;
+    return result;
+  }
+
+  toString() {
+    let string = "";
+    let tempNode = this.head;
+    while (tempNode) {
+      string += `[${tempNode.value}] -> `;
+      tempNode = tempNode.next;
+    }
+    string += `X`;
+    return string;
+  }
+
+  append(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = node;
+    }
+  }
+
+  insertBefore(value, newValue) {
+    let tempNode = this.head;
+    const node = new Node(newValue);
+    if (tempNode.value === value) {
+      node.next = this.head;
+      this.head = node;
+    } else {
+      while (tempNode.next) {
+        if (tempNode.next.value === value) {
+          node.next = tempNode.next;
+          tempNode.next = node;
+          break;
+        } else {
+          tempNode = tempNode.next;
+        }
+      }
+    }
+  }
+
+  insertAfter(value, newValue) {
+    let tempNode = this.head;
+    const node = new Node(newValue);
+    while (tempNode) {
+      if (tempNode.value === value) {
+        node.next = tempNode.next;
+        tempNode.next = node;
+        break;
+      }
+      tempNode = tempNode.next;
+    }
+  }
+
+  kthFromEnd(k) {
+    let tempNode = this.head;
+    let tempArray = [];
+
+    while (tempNode) {
+      tempArray.push(tempNode.value);
+      tempNode = tempNode.next;
+    }
+
+    if (k <= 0) {
+      return "insert positive value ";
+    } else if (k >= tempArray.length) {
+      return "k is larger than the lenth of the linked list";
+    } else {
+      let tempK = tempArray.length - k;
+      let result = tempArray[tempK - 1];
+      return result;
+    }
+  }
+
+  zipLists(list1, list2) {
+    let result = new LinkedList();
+
+    let tempNode1 = list1.head;
+    let tempNode2 = list2.head;
+
+    while (tempNode1 || tempNode2) {
+      if (tempNode1) {
+        result.append(tempNode1.value);
+        tempNode1 = tempNode1.next;
+      }
+      if (tempNode2) {
+        result.append(tempNode2.value);
+        tempNode2 = tempNode2.next;
+      }
+    }
+
+    return `head -> ${result.toString()}`;
+  }
+
+  reverse(list) {
+    let result = new LinkedList();
+    let tempNode = list.head;
+
+    while (tempNode) {
+      result.insert(tempNode.value);
+      tempNode = tempNode.next;
+    }
+
+    return `head -> ${result.toString()}`;
+  }
+}
+
+module.exports = LinkedList;

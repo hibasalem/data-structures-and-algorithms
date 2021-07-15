e685b6f9558d957ce78ca02d7939ef38fe69bfbd
hibasalem <salemhiba.hs@gmail.com>
2021-07-16 02:18:45 +0300

    finished cc11

--- javascript/stack-queue-pseudo/stack-queue-pseudo.js
+++ javascript/stack-queue-pseudo/stack-queue-pseudo.js
@@ -1,4 +1,4 @@
-const Stack = require("../stack");
+const Stack = require("./stack");
 
 class PseudoQueue {
   constructor() {
@@ -6,7 +6,23 @@ class PseudoQueue {
     this.stackB = new Stack();
   }
 
-  enqueue(value) {}
+  enqueue(value) {
+    this.stackA.push(value);
+    return this.stackA.peek();
+  }
 
-  dequeue() {}
+  dequeue() {
+    // while (this.stackB.peek() !== "stack is empty") {
+    //   this.stackA.push(this.stackB.pop());
+    // }
+    if (this.stackB.peek() !== "stack is empty") {
+      return this.stackB.pop();
+    }
+
+    while (this.stackA.peek() !== "stack is empty") {
+      this.stackB.push(this.stackA.pop());
+    }
+    return this.stackB.pop();
+  }
 }
+module.exports = PseudoQueue;
